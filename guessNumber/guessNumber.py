542e49ed5df634ce14d1664b52e5a91bac32be03
Ricardo <qq10043009478@icloud.com>
2020-09-17 13:15:58 +0800

    fix(guessnumber): 添加错误处理，当输入非数字或者超出范围数字时提醒玩家重新输入

--- guessNumber/guessNumber.py
+++ guessNumber/guessNumber.py
@@ -6,13 +6,22 @@ class GuessNumber:
         self.result = None
         self.start = 1
         self.end = 100
-        self.answer = random.randint(1, 100)
+        self.answer = None
 
     def start_game(self):
         print('猜数字游戏开始')
+        self.answer = random.randint(1, 100)
         while self.result != self.answer:
-            self.result = int(input(f'请输入{self.start}到{self.end}的数字：'))
-            if self.result > self.answer:
+            try:
+                self.result = int(input(f'请输入{self.start}到{self.end}的数字：'))
+            except ValueError:
+                print("您输入的不是数字，请再次尝试输入！")
+                continue
+
+            if self.result < self.start or self.result > self.end:
+                print("您输入的数字超出范围，请再次尝试输入！")
+                continue
+            elif self.result > self.answer:
                 self.end = self.result
             elif self.result < self.answer:
                 self.start = self.result
