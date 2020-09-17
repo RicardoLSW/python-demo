fe8562c39916dd6a79a14c3e0d5ae9a37765b0ae
Ricardo <qq10043009478@icloud.com>
2020-09-17 15:15:24 +0800

    feat(guessnumber): 新增记录日志功能，知识点：文件读写、时间格式化

--- guessNumber/guessNumber.py
+++ guessNumber/guessNumber.py
@@ -1,4 +1,5 @@
 import random
+import datetime
 
 
 class GuessNumber:
@@ -7,28 +8,43 @@ class GuessNumber:
         self.start = 1
         self.end = 100
         self.answer = None
+        self.logPath = './guessNumberLog.txt'
 
     def start_game(self):
-        print('猜数字游戏开始')
-        self.answer = random.randint(self.start, self.end)
-        while self.result != self.answer:
-            try:
-                self.result = int(input(f'请输入{self.start}到{self.end}的数字：'))
-            except ValueError:
-                print("您输入的不是数字，请再次尝试输入！")
-                continue
+        """开始猜数字游戏"""
+        try:
+            print('猜数字游戏开始！')
+            self.output_log('猜数字游戏开始！')
+            self.answer = random.randint(self.start, self.end)
+            while self.result != self.answer:
+                try:
+                    self.result = int(input(f'请输入{self.start}到{self.end}的数字：'))
+                    self.output_log(f'请输入{self.start}到{self.end}的数字：{self.result}')
+                    if self.result < self.start or self.result > self.end:
+                        self.output_log('您输入的数字超出范围，请再次尝试输入！')
+                        print('您输入的数字超出范围，请再次尝试输入！')
+                        continue
+                    elif self.result > self.answer:
+                        self.end = self.result
+                    elif self.result < self.answer:
+                        self.start = self.result
+                    else:
+                        self.output_log('猜对了！')
+                        print(f'猜对了！答案是：{self.answer}')
+                        self.output_log('游戏结束！')
+                        print('游戏结束！')
+                        break
+                except ValueError:
+                    self.output_log("您输入的不是数字，请再次尝试输入！")
+                    continue
+        except (KeyboardInterrupt, EOFError):
+            pass
 
-            if self.result < self.start or self.result > self.end:
-                print("您输入的数字超出范围，请再次尝试输入！")
-                continue
-            elif self.result > self.answer:
-                self.end = self.result
-            elif self.result < self.answer:
-                self.start = self.result
-            else:
-                print('猜对了！')
-                print('游戏结束')
-                break
+    def output_log(self, log_message: str):
+        """记录日志"""
+        log_file = open(self.logPath, 'a+')
+        log_file.write(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}：{log_message}\n')
+        log_file.close()
 
 
 if __name__ == '__main__':
